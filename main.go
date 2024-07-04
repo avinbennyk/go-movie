5f13f0e5f9af0dc8ae28df5001052a93cfe38ca2
AVIN BENNY k <avinbenny8@gmail.com>
2024-07-04 10:15:43 +0530

    The movie struct created

--- main.go
+++ main.go
@@ -10,3 +10,20 @@ import{
 	"github.com/gorilla/mux"
 }
 
+type movie struct{
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+
+}
+
+type Director struct{
+	Firstname string `json:"fname"`
+	Lastname string `json:"lname"`
+}
+
+var movies []Movie
+
+
+
